c796a556e86123cc2c4d992566ff0ca35307bfc4
Robert Roach <rjayroach@gmail.com>
2016-09-15 22:19:34 +0800

    add Vagrant + ansible base

---
+++ files/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing Ansible..."
+apt-get install -y apt-transport-https libssl-dev libffi-dev python-dev build-essential python-pip
+pip install -U setuptools cryptography markupsafe
+pip install -U ansible
