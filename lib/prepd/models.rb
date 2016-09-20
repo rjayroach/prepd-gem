e6def5b0cf0ad72f80c46d090cc790e7f7534e25
Robert Roach <rjayroach@gmail.com>
2016-09-20 16:37:57 +0800

    create project in 'test' mode

--- lib/prepd/models.rb
+++ lib/prepd/models.rb
@@ -1,40 +1,48 @@
 module Prepd
-  # Client has_many projects; Project has_many applications
   class Client < ActiveRecord::Base
     attr_accessor :data_dir
     has_many :projects
     has_many :applications, through: :projects
+
     before_validation :set_defaults
     validates :name, :path, presence: true
+
     after_create :setup
-    # after_find :set_current_client
 
     def set_defaults
-      self.data_dir ||= ENV['DATA_DIR']
-      self.path = "#{data_dir}/#{name}"
+      self.path = "#{Prepd.options['DATA_DIR']}/#{name}"
     end
 
     def setup
       FileUtils.mkdir_p(path)
     end
-
-    def set_current_client
-      STDOUT.puts 'after find'
-      Prepd.current_client = self
-    end
   end
 
+
   class Project < ActiveRecord::Base
+    attr_accessor :mode
     belongs_to :client, required: true
     has_many :applications
+
     validates :name, presence: true, uniqueness: { scope: :client }
-    after_create :setup
+
+    after_create :setup, unless: "mode.eql?('test')"
+    after_create :setup_test, if: "mode.eql?('test')"
 
     #
     # Copy files from the prepd/files directory
     #
     def setup
       FileUtils.cp_r(files_path, path)
+    end
+
+    def setup_test
+      FileUtils.mkdir_p(path)
+      Dir.chdir(path) do
+        %w(Vagrantfile bootstrap.sh).each do |link|
+          FileUtils.ln_s("#{files_path}/#{link}", link)
+        end
+      end
     end
 
     def files_path
@@ -47,10 +55,11 @@ module Prepd
   end
 
 
-
   class Application < ActiveRecord::Base
     belongs_to :project, required: true
+
     validates :name, presence: true, uniqueness: { scope: :project }
+
     after_create :setup
 
     def setup
